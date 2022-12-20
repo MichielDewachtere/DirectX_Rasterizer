f10fce5d66c3a7c2454d52faeaf501dfbcb0bfa4
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2022-12-20 14:37:31 +0100

    Added Mesh Class
    Class for mesh representation, in other words, our data buffers.

---
+++ source/Mesh.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "Vector3.h"
+#include "ColorRGB.h"
+#include <vector>
+
+using namespace dae;
+class Effect;
+
+struct Vertex
+{
+	Vector3 position;
+	ColorRGB color;
+};
+
+class Mesh final
+{
+public:
+	Mesh(ID3D11Device* pDevice, const std::vector<Vertex>& vertices, const std::vector<uint32_t>& indices);
+	~Mesh();
+
+	Mesh(const Mesh& other) = delete;
+	Mesh& operator=(const Mesh& rhs) = delete;
+	Mesh(Mesh&& other) = delete;
+	Mesh& operator=(Mesh&& rhs) = delete;
+
+	void Render(ID3D11DeviceContext* pDeviceContext) const;
+
+private:
+	ID3D11Buffer* m_pVertexBuffer{};
+	Effect* m_pEffect;
+	ID3D11InputLayout* m_pVertexLayout{};
+	uint32_t m_AmountIndices;
+	ID3D11Buffer* m_pIndexBuffer{};
+
+	std::vector<Vertex> m_Vertices;
+};
+
