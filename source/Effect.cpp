14b5c65765ef05d63431237b8f3bdfa6f9c4276b
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2022-12-23 21:25:01 +0100

    Updated Effect Class
    We can now pass extra variables to our shader

--- source/Effect.cpp
+++ source/Effect.cpp
@@ -17,6 +17,9 @@ Effect::Effect(ID3D11Device* pDevice, const std::wstring& assetFile)
 
 Effect::~Effect()
 {
+	if (m_pGlossinessMapVariable) m_pGlossinessMapVariable->Release();
+	if (m_pSpecularMapVariable) m_pSpecularMapVariable->Release();
+	if (m_pNormalMapVariable) m_pNormalMapVariable->Release();
 	if (m_pDiffuseMapVariable) m_pDiffuseMapVariable->Release();
 	if (m_pMatWorldViewProjVariable) m_pMatWorldViewProjVariable->Release();
 
@@ -37,16 +40,65 @@ void Effect::SetDiffuseMap(const std::string& filePath, ID3D11Device* pDevice) c
 	delete diffuseTexture;
 }
 
+void Effect::SetNormalMap(const std::string& filePath, ID3D11Device* pDevice) const
+{
+	const Texture* normalTexture = Texture::LoadFromFile(filePath, pDevice);
+
+	if (m_pNormalMapVariable)
+		m_pNormalMapVariable->SetResource(normalTexture->GetShaderResourceView());
+
+	delete normalTexture;
+}
+
+void Effect::SetSpecularMap(const std::string& filePath, ID3D11Device* pDevice) const
+{
+	const Texture* specularTexture = Texture::LoadFromFile(filePath, pDevice);
+
+	if (m_pSpecularMapVariable)
+		m_pSpecularMapVariable->SetResource(specularTexture->GetShaderResourceView());
+
+	delete specularTexture;
+}
+
+void Effect::SetGlossinessMap(const std::string& filePath, ID3D11Device* pDevice) const
+{
+	const Texture* glossinessTexture = Texture::LoadFromFile(filePath, pDevice);
+
+	if (m_pGlossinessMapVariable)
+		m_pGlossinessMapVariable->SetResource(glossinessTexture->GetShaderResourceView());
+
+	delete glossinessTexture;
+}
+
 void Effect::InitVariables()
 {
 	m_pMatWorldViewProjVariable = m_pEffect->GetVariableByName("gWorldViewProj")->AsMatrix();
 	if (m_pMatWorldViewProjVariable->IsValid() == false)
 		std::wcout << L"m_pMatWorldViewProjVariable not valid!\n";
 
+	m_pMatWorldVariable = m_pEffect->GetVariableByName("gWorldMatrix")->AsMatrix();
+	if (m_pMatWorldVariable->IsValid() == false)
+		std::wcout << L"m_pMatWorldVariable not valid!\n";
+
+	m_pMatInvViewVariable = m_pEffect->GetVariableByName("gInvViewMatrix")->AsMatrix();
+	if (m_pMatInvViewVariable->IsValid() == false)
+		std::wcout << L"m_pMatInvViewVariable not valid!\n";
+
 	m_pDiffuseMapVariable = m_pEffect->GetVariableByName("gDiffuseMap")->AsShaderResource();
 	if (!m_pDiffuseMapVariable->IsValid())
 		std::wcout << L"m_pDiffuseMapVariable not valid!\n";
 
+	m_pNormalMapVariable = m_pEffect->GetVariableByName("gNormalMap")->AsShaderResource();
+	if (!m_pDiffuseMapVariable->IsValid())
+		std::wcout << L"m_pNormalMapVariable not valid!\n";
+
+	m_pSpecularMapVariable = m_pEffect->GetVariableByName("gSpecularMap")->AsShaderResource();
+	if (!m_pDiffuseMapVariable->IsValid())
+		std::wcout << L"m_pSpecularMapVariable not valid!\n";
+
+	m_pGlossinessMapVariable = m_pEffect->GetVariableByName("gGlossinessMap")->AsShaderResource();
+	if (!m_pDiffuseMapVariable->IsValid())
+		std::wcout << L"m_pGlossinessMapVariable not valid!\n";
 }
 
 void Effect::InitTechniques()
--- source/Effect.h
+++ source/Effect.h
@@ -19,11 +19,23 @@ public:
 	ID3DX11EffectTechnique* GetLinearTechnique() const { return m_pLinearTechnique; }
 	ID3DX11EffectTechnique* GetAnisotropicTechnique() const { return m_pAnisotropicTechnique; }
 
-	void SetMatrixVariable(const Matrix& newValue) const
+	void SetWorldViewProjectionMatrixVariable(const Matrix& newValue) const
 	{
 		m_pMatWorldViewProjVariable->SetMatrix(reinterpret_cast<const float*>(&newValue));
 	}
+	void SetWorldMatrixVariable(const Matrix& newValue) const
+	{
+		m_pMatWorldVariable->SetMatrix(reinterpret_cast<const float*>(&newValue));
+	}
+	void SetInvViewMatrixVariable(const Matrix& newValue) const
+	{
+		m_pMatInvViewVariable->SetMatrix(reinterpret_cast<const float*>(&newValue));
+	}
+
 	void SetDiffuseMap(const std::string& filePath, ID3D11Device* pDevice) const;
+	void SetNormalMap(const std::string& filePath, ID3D11Device* pDevice) const;
+	void SetSpecularMap(const std::string& filePath, ID3D11Device* pDevice) const;
+	void SetGlossinessMap(const std::string& filePath, ID3D11Device* pDevice) const;
 
 private:
 	ID3DX11Effect* m_pEffect;
@@ -34,7 +46,14 @@ private:
 	void InitTechniques();
 
 	ID3DX11EffectMatrixVariable* m_pMatWorldViewProjVariable;
+	ID3DX11EffectMatrixVariable* m_pMatWorldVariable;
+	ID3DX11EffectMatrixVariable* m_pMatInvViewVariable;
+
 	ID3DX11EffectShaderResourceVariable* m_pDiffuseMapVariable;
+	ID3DX11EffectShaderResourceVariable* m_pNormalMapVariable;
+	ID3DX11EffectShaderResourceVariable* m_pSpecularMapVariable;
+	ID3DX11EffectShaderResourceVariable* m_pGlossinessMapVariable;
+
 	void InitVariables();
 
 	static ID3DX11Effect* LoadEffect(ID3D11Device* pDevice, const std::wstring& assetFile);
